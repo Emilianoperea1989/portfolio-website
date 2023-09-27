2b3b2a16a7c1a3d5fa8ea2ab91a24c0bc7eebdb8
Emiliano Perea <emilianoperea10@gmail.com>
2023-09-27 13:56:35 -0300

    cambiando etiquetas Image por img

--- src/compontents/AboutSection.jsx
+++ src/compontents/AboutSection.jsx
@@ -54,7 +54,7 @@ const AboutSection = () => {
   return (
     <section className="text-white" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image src="https://res.cloudinary.com/duqilis2i/image/upload/v1695833437/Portfolio/Emi_definitivo_n9z2lt.png" width={500} height={500} alt="ilutration"/>
+        <img src="https://res.cloudinary.com/duqilis2i/image/upload/v1695833437/Portfolio/Emi_definitivo_n9z2lt.png" width={500} height={500} alt="ilutration"/>
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
--- src/compontents/HeroSection.jsx
+++ src/compontents/HeroSection.jsx
@@ -1,6 +1,5 @@
 "use client";
 import React from 'react'
-import Image from 'next/image'
 import { TypeAnimation } from 'react-type-animation';
 import Link from "next/link";
 import { motion } from 'framer-motion';
@@ -54,7 +53,7 @@ const HeroSection = () => {
             transition={{duration:0.5}} 
              className='col-span-4 place-self-center mt-4 lg:mt-0'>
                 <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[300px] lg:h-[300px] relative">
-                    <Image src="https://res.cloudinary.com/duqilis2i/image/upload/v1695833433/Portfolio/emi_sqm9va.png"
+                    <img src="https://res.cloudinary.com/duqilis2i/image/upload/v1695833433/Portfolio/emi_sqm9va.png"
                         alt="Emiliano"
                         className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2'
                         width={400}
